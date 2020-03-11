5a321df031108ffe8bbf7818c63ed02563a7e29a
pstep <p.stephan@gmx.at>
2020-03-11 09:41:38 +0100

    Netzwerkklasse angefangen, grundlegende Attribute, die für die Übertragung notwendig sind, mit Konstruktor angelegt.

--- app/src/main/java/com/example/se2einzelprojekt/MainActivity.java
+++ app/src/main/java/com/example/se2einzelprojekt/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.se2einzelprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    class Netzwerkübertragung extends AsyncTask{
+        String serverDomain;
+        String portNummer;
+        String matrikelnummer;
+
+        public Netzwerkübertragung(String serverDomain, String portNummer, String matrikelnummer) {
+            this.serverDomain = serverDomain;
+            this.portNummer = portNummer;
+            this.matrikelnummer = matrikelnummer;
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            return null;
+        }
     }
 }
